aef3731d66c1f5aa5c6192e07db56d1f79dab4e6
Leonardo Aguiar <leonardovenom9@gmail.com>
2026-09-02 00:41:53 -0300

    docs: remove codificacao da branch docs

--- e-lixo-zero/BackEnd/src/main/java/br/fai/lds/e_lixo_zero/exceptions/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package br.fai.lds.e_lixo_zero.exceptions;
-
-public class BadRequestException extends ApiException {
-
-    public BadRequestException(final String message) {
-        super(message, 400);
-    }
-}
--- e-lixo-zero/BackEnd/src/main/java/br/fai/lds/e_lixo_zero/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.fai.lds.e_lixo_zero.exceptions;
-
-public class ResourceNotFoundException extends ApiException {
-
-    public ResourceNotFoundException(final String message) {
-        super(message, 404);
-    }
-}
